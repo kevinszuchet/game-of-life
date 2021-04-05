0ffaad3b678a5b19829f5b82c7399f6e875ac407
Kevin Szuchet <kevinszuchet@gmail.com>
2021-04-05 20:06:40 -0300

    1st draw with cells

--- game_of_life.py
+++ game_of_life.py
@@ -18,4 +18,17 @@ dimCW = width / nxC
 dimCH = height / nyC
 
 while True:
-    pass
+
+    for x in range(0, nxC):
+        for y in range(0, nyC):
+            # Create the polygon to draw
+            poly = [
+                (x * dimCW, y * dimCH),
+                ((x + 1) * dimCW, y * dimCH),
+                ((x + 1) * dimCW, (y + 1) * dimCH),
+                (x * dimCW, (y + 1) * dimCH)
+            ]
+
+            pygame.draw.polygon(screen, (128, 128, 128), poly, width=1)
+
+    pygame.display.flip()
